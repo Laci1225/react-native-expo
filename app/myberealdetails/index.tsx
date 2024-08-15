7a93b705c3650ed9689788d725038be8f9f90594
Nagy László Levente <nagylacika2002@gmail.com>
2024-08-15 02:27:05 +0200

    Comment section is now absolute to the screen

--- app/myberealdetails/index.tsx
+++ app/myberealdetails/index.tsx
@@ -16,7 +16,7 @@ import {uint8ArrayToBase64} from "@/customComponents/uint8ArrayToBase64";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {client} from "@/api/client";
 import {AxiosError} from "axios";
-import {Entypo, FontAwesome} from "@expo/vector-icons";
+import {Entypo, FontAwesome, MaterialCommunityIcons} from "@expo/vector-icons";
 
 export default function BeRealDetailsScreen() {
     const router = useRouter();
@@ -67,92 +67,97 @@ export default function BeRealDetailsScreen() {
     };
 
     return (
-        <ScrollView style={styles.container}>
-            <View style={{paddingHorizontal: 10}}>
-            <View style={styles.header}>
-                <Entypo style={{width: "15%"}} name="chevron-left" size={24} color="white"
-                        onPress={() => router.back()}/>
-                <View style={{width: "70%", alignItems: "center"}}>
-                    <Text style={styles.headerText}>My BeReal.</Text>
+        <View style={styles.container}>
+            <ScrollView style={styles.container}>
+                <View style={{paddingHorizontal: 10}}>
+                    <View style={styles.header}>
+                        <Entypo style={{width: "15%"}} name="chevron-left" size={24} color="white"
+                                onPress={() => router.back()}/>
+                        <View style={{width: "70%", alignItems: "center"}}>
+                            <Text style={styles.headerText}>My BeReal.</Text>
+                            <View>
+                                <Text
+                                    style={styles.headerSmallerText}>Date: {new Date(myBeReal.dateCreated).toLocaleString()}</Text>
+                            </View>
+                        </View>
+                        <View style={{width: "15%", flexDirection: "row"}}>
+                            <Entypo style={{marginRight: 15}} name="share" size={24} color="white"/>
+                            <Entypo name="dots-three-vertical" size={24} color="white"/>
+                        </View>
+                    </View>
+                    <View style={styles.imagesContainer}>
+                        <View style={styles.imageWrapper}>
+                            <Image
+                                source={{uri: `data:image/jpeg;base64,${uint8ArrayToBase64(isSwapped ? myBeReal.frontPhoto : myBeReal.backPhoto)}`}}
+                                style={styles.capturedImageBig}
+                            />
+                            <Pressable onPress={handleImagePress} style={styles.touchableWrapper}>
+                                <Image
+                                    source={{uri: `data:image/jpeg;base64,${uint8ArrayToBase64(isSwapped ? myBeReal.backPhoto : myBeReal.frontPhoto)}`}}
+                                    style={styles.capturedImageSmall}
+                                />
+                            </Pressable>
+                        </View>
+                        <Text style={{color: "white", marginTop: 20, fontSize: 16, fontWeight: "bold"}}>Add
+                            caption...</Text>
+                        <Text style={{color: "grey", marginVertical: 5}}>Only visible to your friends</Text>
+                        <View style={{
+                            backgroundColor: "rgba(80,80,80,0.5)", flexDirection: "row", marginTop: 5,
+                            paddingHorizontal: 15, paddingVertical: 4, borderRadius: 25, alignItems: "center"
+                        }}>
+                            <FontAwesome name="location-arrow" size={24} color="white"/>
+                            <Text style={{color: "white", paddingLeft: 8}}>{myBeReal.location}</Text>
+                        </View>
+                    </View>
+                    <View style={{borderBottomColor: 'gray', borderBottomWidth: 1, marginVertical: 15}}/>
                     <View>
-                        <Text
-                            style={styles.headerSmallerText}>Date: {new Date(myBeReal.dateCreated).toLocaleString()}</Text>
+                        <FlatList data={[1, 2, 3, 4]} renderItem={
+                            ({item}) => (
+                                <Image source={require('@/assets/images/icon.png')}
+                                       style={{width: 65, height: 65, borderRadius: 50, marginHorizontal: 5}}/>
+                            )
+                        }
+                                  style={{paddingHorizontal: 5}} horizontal={true}
+                        />
                     </View>
                 </View>
-                <View style={{width: "15%", flexDirection: "row"}}>
-                    <Entypo style={{marginRight: 15}} name="share" size={24} color="white"/>
-                    <Entypo name="dots-three-vertical" size={24} color="white"/>
+                <View style={{borderBottomColor: 'gray', borderBottomWidth: 1, marginVertical: 15}}/>
+                <View>
+                    <FlatList data={[1, 2]} renderItem={
+                        ({item}) => (
+                            <View style={{flexDirection: "row", marginVertical: 5}}>
+                                <Image source={require('@/assets/images/icon.png')}
+                                       style={{width: 50, height: 50, borderRadius: 25, marginHorizontal: 5}}/>
+                                <Text style={{color: "white"}}>Comment</Text>
+                            </View>
+                        )
+                    }
+                              style={{paddingHorizontal: 5}}/>
                 </View>
-            </View>
-            <View style={styles.imagesContainer}>
-                <View style={styles.imageWrapper}>
-                    <Image
-                        source={{uri: `data:image/jpeg;base64,${uint8ArrayToBase64(isSwapped ? myBeReal.frontPhoto : myBeReal.backPhoto)}`}}
-                        style={styles.capturedImageBig}
-                    />
-                    <Pressable onPress={handleImagePress} style={styles.touchableWrapper}>
-                        <Image
-                            source={{uri: `data:image/jpeg;base64,${uint8ArrayToBase64(isSwapped ? myBeReal.backPhoto : myBeReal.frontPhoto)}`}}
-                            style={styles.capturedImageSmall}
-                        />
-                    </Pressable>
-                </View>
-                <Text style={{color: "white", marginTop: 20, fontSize: 16, fontWeight: "bold"}}>Add caption...</Text>
-                <Text style={{color: "grey", marginVertical: 5}}>Only visible to your friends</Text>
-                <View style={{
-                    backgroundColor: "rgba(80,80,80,0.5)", flexDirection: "row", marginTop: 5,
-                    paddingHorizontal: 15, paddingVertical: 4, borderRadius: 25, alignItems: "center"
-                }}>
-                    <FontAwesome name="location-arrow" size={24} color="white"/>
-                    <Text style={{color: "white", paddingLeft: 8}}>{myBeReal.location}</Text>
-                </View>
-            </View>
-            <View style={{borderBottomColor: 'gray', borderBottomWidth: 1, marginVertical: 15}}/>
-            <View>
-                {
-                    // reaction section with round pictures now with sample white pics with FlatList
-                }
-                <FlatList data={[1, 2, 3, 4]} renderItem={
-                    ({item}) => (
-                        <Image source={require('@/assets/images/icon.png')}
-                               style={{width: 65, height: 65, borderRadius: 50, marginHorizontal: 5}}/>
-                    )
-                }
-                          style={{paddingHorizontal: 5}} horizontal={true}
-                />
-            </View>
-            </View>
-            <View style={{borderBottomColor: 'gray', borderBottomWidth: 1, marginVertical: 15}}/>
-            <View>
-                <FlatList data={[1, 2]} renderItem={
-                    ({item}) => (
-                        <View style={{flexDirection: "row", marginVertical: 5}}>
-                            <Image source={require('@/assets/images/icon.png')}
-                                   style={{width: 50, height: 50, borderRadius: 25, marginHorizontal: 5}}/>
-                            <Text style={{color: "white"}}>Comment</Text>
-                        </View>
-                    )
-                }
-                          style={{paddingHorizontal: 5}}/>
-            </View>
+
+            </ScrollView>
             <View style={{
                 position: "absolute",
-                bottom: 50,
-                borderColor: "white",
-                borderWidth: 1,
+                bottom: 0,
                 width: "100%",
-                height: 50
+                height: 50,
+                backgroundColor: "black",
             }}>
-                <TextInput
-                    placeholder="Add a comment..."
-                    style={{justifyContent: "center", padding: 10, width: "100%", height: "100%", fontSize: 16,}}
-                    value={""}
-                    onChangeText={() => {
-                    }}
-                />
+                <View style={{borderBottomColor: 'gray', borderBottomWidth: 1}}/>
+            <View style={{flexDirection:"row", justifyContent: "space-between", alignItems: "center"}}>
+            <TextInput
+                placeholder="Add a comment..."
+                placeholderTextColor={"rgba(100,100,100,1)"}
+                style={{fontSize: 22, color: "white", paddingHorizontal: 20, height: 40, width: "90%"}}
+                onChangeText={() => {
+                }}
+            />
+                <MaterialCommunityIcons name="send-circle" size={40} color="blue" />
 
             </View>
-        </ScrollView>
+
+            </View>
+        </View>
     );
 }
 
@@ -160,7 +165,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#000',
-        paddingTop: 16,
+        paddingTop: 5,
     },
     header: {
         flexDirection: 'row',
